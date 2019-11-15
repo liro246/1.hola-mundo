a85e49bdb1faa2635fe910bb7a3581a81b8d7cb4
jume30 <jume30@gmail.com>
2019-11-15 11:49:13 -0500

    Update body.component.ts

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -8,11 +8,7 @@ export class BodyComponent{
 
     mostrar=true;
     frase:any={
-<<<<<<< Updated upstream
-        mensaje:"Un gran poder requiere una gran responsabilidad - Julián Cambio",
-=======
         mensaje:"Prueba mensaje Lino MK",
->>>>>>> Stashed changes
         autor:"Ben Parker"
     }
 
